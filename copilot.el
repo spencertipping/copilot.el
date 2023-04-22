74b30cabef05fb9a2ff5b1ec654a001bdb49404f
Spencer Tipping <spencer.tipping@gmail.com>
2023-04-22 11:03:27 -0600

    Downgraded required emacs version

--- copilot.el
+++ copilot.el
@@ -1,6 +1,6 @@
 ;;; copilot.el --- An unofficial Copilot plugin for Emacs  -*- lexical-binding:t -*-
 
-;; Package-Requires: ((emacs "27.2") (s "1.12.0") (dash "2.19.1") (editorconfig "0.8.2") (jsonrpc "1.0.14"))
+;; Package-Requires: ((emacs "27.1") (s "1.12.0") (dash "2.19.1") (editorconfig "0.8.2") (jsonrpc "1.0.14"))
 
 ;;; Code:
 (require 'cl-lib)
